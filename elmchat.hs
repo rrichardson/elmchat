d2f8ac07da0f9e740f42a64fa6f4c252fe3268b3
Rick Richardson <rrichardson6@bloomberg.net>
2013-06-03 16:27:44 -0400

    working http and websockets baseline

--- elmchat.hs
+++ elmchat.hs
@@ -1,169 +1,127 @@
-websockets example
-==================
+{-# LANGUAGE OverloadedStrings, TypeFamilies, DeriveDataTypeable, TemplateHaskell #-}
 
-This is the Haskell implementation of the example for the WebSockets library. We
-implement a simple multi-user chat program. A live demo of the example is
-available [here](http://jaspervdj.be/websockets-example). In order to understand
-this example, keep the [reference](http://jaspervdj.be/websockets/reference)
-nearby to check out the functions we use.
+module Main where
 
-> {-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
-> import Data.Char (isPunctuation, isSpace)
-> import Data.Monoid (mappend)
-> import Data.Text (Text)
-> import Control.Exception (fromException)
-> import Control.Monad (forM_)
-> import Control.Concurrent (MVar, newMVar, modifyMVar_, readMVar)
-> import Control.Monad.IO.Class (liftIO)
-> import qualified Data.Text as T
-> import qualified Data.Text.IO as T
+import Web.Scotty
 
-> import qualified Network.WebSockets as WS
-> import qualified Network.Wai
-> import qualified Network.Wai.Handler.Warp as Warp
-> import qualified Network.Wai.Handler.WebSockets as WaiWS
-> import qualified Network.Wai.Application.Static as Static
-> import Data.FileEmbed (embedDir)
+import Data.Default (def)
+import Data.Typeable
+import qualified Data.Map as Map
+import qualified Data.Aeson as J
 
-We represent a client by his username and a 'WS.Sender'. We can use this sender
-to asynchronously send 'Text' to the client later. Note that using `WS.Hybi00`
-here does not imply that our server is only compatible with the `hybi-00`
-version of the protocol, for more details on this, see the
-[Network.WebSockets](http://jaspervdj.be/websockets/reference/Network-WebSockets.html) 
-reference.
+-- Network
+import Network.Wai.Middleware.Static
+import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Handler.Warp (settingsPort, settingsIntercept )
+import Network.HTTP.Types  (status404, status200)
+import qualified Network.WebSockets             as WS
+import qualified Network.Wai.Handler.WebSockets as WS
 
-> type Client = (Text, WS.Sink WS.Hybi00)
+-- Database
+import Data.Acid
+import Data.SafeCopy
+import qualified Data.Map as M
+import Control.Monad.Reader (ask)
+import Control.Monad.IO.Class (liftIO)
 
-The state kept on the server is simply a list of connected clients. We've added
-an alias and some utility functions, so it will be easier to extend this state
-later on.
+import qualified Control.Monad.State as S
+import Control.Lens (makeLenses)
+import Control.Monad (forever)
+import Control.Concurrent (forkIO, threadDelay)
 
-> type ServerState = [Client]
+import Data.Time.Clock (getCurrentTime)
+data Clock = Clock
+------------------------------------------------------------------------------
 
-Create a new, initial state
+type Key = String
+type Value = String
 
-> newServerState :: ServerState
-> newServerState = []
+data Database = Database !(Map.Map Key Value)
+    deriving (Show, Ord, Eq, Typeable)
 
-Get the number of active clients
+$(deriveSafeCopy 0 'base ''Database)
+makeLenses ''Database
 
-> numClients :: ServerState -> Int
-> numClients = length
+------------------------------------------------------
 
-Check if a user already exists (based on username)
+insertKey :: Key -> Value -> Update Database ()
+insertKey key value
+    = do Database m <- S.get
+         S.put (Database (Map.insert key value m))
 
-> clientExists :: Client -> ServerState -> Bool
-> clientExists client = any ((== fst client) . fst)
+lookupKey :: Key -> Query Database (Maybe Value)
+lookupKey key
+    = do Database m <- ask
+         return (Map.lookup key m)
 
-Add a client (first, you should verify the client is not already connected using
-'clientExists')
+deleteKey :: Key -> Update Database ()
+deleteKey key
+    = do Database m <- S.get
+         S.put (Database (Map.delete key m))
 
-> addClient :: Client -> ServerState -> ServerState
-> addClient client clients = client : clients
+allKeys :: Int -> Query Database [(Key, Value)]
+allKeys limit
+    = do Database m <- ask
+         return $ take limit (M.toList m)
 
-Remove a client
+$(makeAcidic ''Database ['insertKey, 'lookupKey, 'allKeys, 'deleteKey])
 
-> removeClient :: Client -> ServerState -> ServerState
-> removeClient client = filter ((/= fst client) . fst)
+------------------------------------------------------------------------------
 
-Send a message to all clients, and log it on stdout.
+config db = def { verbose = 0
+           , settings = (settings def) { settingsPort = 4000, settingsIntercept = WS.intercept (socketsmain db) }
+           }
 
-> broadcast :: Text -> ServerState -> IO ()
-> broadcast message clients = do
->     T.putStrLn message
->     forM_ clients $ \(_, sink) -> WS.sendSink sink $ WS.textData message
+fixtures :: M.Map String String
+fixtures = M.fromList [("one", "1"), ("two", "2"), ("three", "3")]
 
-The main function first creates a new state for the server, then spawns the
-actual server. For this purpose, we use the simple server provided by
-'WS.runServer'.
+------------------------------------------------------------------------------
+-- MAIN MAIN
 
-> main :: IO ()
-> main = do
->     putStrLn "http://localhost:9160/client.html"
->     state <- newMVar newServerState
->     Warp.runSettings Warp.defaultSettings
->       { Warp.settingsPort = 9160
->       , Warp.settingsIntercept = WaiWS.intercept (application state)
->       } staticApp
+main :: IO ()
+main = do
+    putStrLn "Loading Database."
+    db <- openLocalStateFrom "db/" (Database fixtures)
 
-> staticApp :: Network.Wai.Application
-> staticApp = Static.staticApp $ Static.embeddedSettings $(embedDir "static")
+    putStrLn "Starting HTTP Server."
+    scottyOpts (config db) (httpmain db)
 
-When a client connects, we accept the connection, regardless of the path.
 
-> application :: MVar ServerState -> WS.Request -> WS.WebSockets WS.Hybi00 ()
-> application state rq = do
->     WS.acceptRequest rq
+------------------------------------------------------------------------------
+-- HTTP MAIN
 
-We log some information here: in particular, we are interested in the protocol
-version our client is using (for debugging purposes).
+httpmain :: AcidState (EventState AllKeys) -> ScottyM ()
+httpmain database = do
+        middleware logStdoutDev
+        middleware $ staticPolicy (noDots >-> addBase "static")
 
->     WS.getVersion >>= liftIO . putStrLn . ("Client version: " ++)
+        get "/" $ file "index.html"
 
-If we want to be able to send data to this client later, from another thread, we
-obtain a sink. We will add this to the server state later.
+        get "/read/" $ do
+            result <- liftIO $ query database (AllKeys 10)
+            json result
 
->     sink <- WS.getSink
+        get "/read/:key" $ do
+            key <- param "key"
+            result <- liftIO $ query database (LookupKey key)
+            case result of
+               Nothing    -> status status404
+               Just value -> json value
 
-When a client is succesfully connected, we read the first message. This should
-be in the format of "Hi, I am Jasper", where Jasper is the requested username.
+        delete "/delete/:key" $ do
+            key <- param "key"
+            _   <- liftIO $ update database (DeleteKey key)
+            status status200
 
->     msg <- WS.receiveData
->     clients <- liftIO $ readMVar state
->     case msg of
+        post "/update/" $ do
+            (key, val) <- jsonData
+            _          <- liftIO $ update database (InsertKey key val)
+            status status200
 
-Check that the first message has the right format
+------------------------------------------------------------------------------
+-- Websockets MAIN
 
->         _   | not (prefix `T.isPrefixOf` msg) ->
->                 WS.sendTextData ("Wrong announcement" :: Text)
 
-Check the validity of the username
-
->             | any ($ fst client)
->                 [T.null, T.any isPunctuation, T.any isSpace] ->
->                     WS.sendTextData ("Name cannot " `mappend`
->                         "contain punctuation or whitespace, and " `mappend`
->                         "cannot be empty" :: Text)
-
-Check that the given username is not already taken
-
->             | clientExists client clients ->
->                 WS.sendTextData ("User already exists" :: Text)
-
-All is right!
-
->             | otherwise -> do
-
-We send a "Welcome!", according to our own little protocol. We add the client to
-the list and broadcast the fact that he has joined. Then, we give control to the
-'talk' function.
-
->                liftIO $ modifyMVar_ state $ \s -> do
->                    let s' = addClient client s
->                    WS.sendSink sink $ WS.textData $
->                        "Welcome! Users: " `mappend`
->                        T.intercalate ", " (map fst s)
->                    broadcast (fst client `mappend` " joined") s'
->                    return s'
->                talk state client
->           where
->             prefix = "Hi! I am "
->             client = (T.drop (T.length prefix) msg, sink)
-
-The talk function continues to read messages from a single client until he
-disconnects. All messages are broadcasted to the other clients.
-
-> talk :: WS.Protocol p => MVar ServerState -> Client -> WS.WebSockets p ()
-> talk state client@(user, _) = flip WS.catchWsError catchDisconnect $ do
->     msg <- WS.receiveData
->     liftIO $ readMVar state >>= broadcast
->         (user `mappend` ": " `mappend` msg)
->     talk state client
->   where
->     catchDisconnect e = case fromException e of
->         Just WS.ConnectionClosed -> liftIO $ modifyMVar_ state $ \s -> do
->             let s' = removeClient client s
->             broadcast (user `mappend` " disconnected") s'
->             return s'
->         _ -> return ()
-
+socketsmain :: AcidState (EventState AllKeys) -> WS.Request -> WS.WebSockets WS.Hybi00 ()
+socketsmain database rq = do
+     WS.acceptRequest rq
